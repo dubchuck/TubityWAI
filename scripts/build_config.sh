330f22ee1e3b7ba7a2a1a024399d8bac933c0af9
jeremy@dubchuck.com <jeremy@dubchuck.com>
2026-08-04 20:09:10 -0700

    Remove original scripts in preparation for submodule

--- scripts/build_config.sh
+++
@@ -1,7 +0,0 @@
-# Cached build variables
-TARGET_PLATFORM="ios"
-APP_NAME="TubityX"
-BUNDLE_ID="com.dubchuck.tubityx"
-TEAM_ID="8W7DKFU852"
-SKU="tubityx-sku-001"
-CREDENTIALS_JSON="appstore_ios_fastland_credentials.json"
